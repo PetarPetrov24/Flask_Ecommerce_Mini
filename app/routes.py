8bacd1505608b8f5528d6be091a191471474d9d7
Petar Petrov <ppp1808@gmail.com>
2026-02-22 19:19:23 +0200

    Imported jsonify from flask

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import os
 from werkzeug.utils import secure_filename
-from flask import render_template, request, redirect, url_for, session, flash, abort
+from flask import render_template, request, redirect, url_for, session, flash, abort, jsonify
 from flask_login import current_user, login_required, login_user, logout_user
 from .models import Product, User, Order, OrderItem
 from .forms import LoginForm, RegisterForm, AddProduct, RemoveFromCartForm
